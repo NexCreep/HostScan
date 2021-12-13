e2e8f9c299bd6a905c7dcd0b720bbdde1418fdab
NexCreep <codenexcreep@gmail.com>
2021-12-13 21:08:24 +0100

    Initial Commit

---
+++ HostScan.py
@@ -0,0 +1,110 @@
+import nmap
+from threading import Thread
+from time import sleep
+
+endLoad = False
+
+
+
+class Loading_thread:
+      
+    def __init__(self):
+        self._running = True
+      
+    def terminate(self):
+        self._running = False
+      
+    def run(self):
+        MAX_TIME = 120
+        bar_char = "*"
+        i = 1
+        while self._running and i <= MAX_TIME:
+            print(bar_char, end="", flush=True)
+            i += 1
+            sleep(1)
+        if i >= MAX_TIME:
+            print(f"\n[-] This is taking a may while. Take a look to arguments you introduce. Use Ctrl+C to finish proccess (Timeout)")
+        print(f"(Proccess took {i} sec)", end="", flush=True)
+
+def scan_host(ip: str, moreInfo: bool):
+    print('\n')
+    print(f"[!] Analyzing subnet with mask {ip}")
+
+    load = Loading_thread()
+    t_load = Thread(target=load.run)
+    t_load.start()
+
+    try:
+        nport = nmap.PortScanner()
+
+        nport.scan(hosts= f'{ip}', arguments='-n -sn -PE -PA21,23,80,3389')
+
+        load.terminate()
+
+        print()
+        print(f"[!] Discovered {len(nport.all_hosts())} host UP")
+
+        if not moreInfo:
+            for x in nport.all_hosts():
+                host_info = nport[x]
+                status = host_info["status"]
+
+                if status["reason"] != "localhost-response":
+                    print(f'[+] Host with IP {x} is {status["state"].upper()} \n')
+        else:
+            for x in nport.all_hosts():
+                host_info = nport[x]
+                status = host_info["status"]
+
+                if status["reason"] != "localhost-response":
+                    vendor = host_info["vendor"]
+                    addresses = host_info["addresses"]
+
+                    print(f'[+] Host with IP {x} is {status["state"].upper()}')
+                    print(f'\t└\\ Vendor => {len(vendor) > 0 and vendor[addresses["mac"]] or "NO INFORMATION"}')
+                    print(f'\t└\\ Addresses => Ip: {addresses["ipv4"]} | {addresses["mac"] and "MAC: " + addresses["mac"] or "NO MAC ADDRESS"}')
+                    print(f'\t└\\ State Reason => {status["state"].upper()} because {status["reason"].upper()}')
+                    print()
+
+        sys.exit(0)
+    except nmap.nmap.PortScannerError as e:     
+        print("[-] You must have installed and added to path nmap from this url -> https://nmap.org/download.html")
+
+    except KeyboardInterrupt as e:
+        load.terminate()
+        print("\n[-] Exit by user")
+
+    except Exception as e:
+        print(e)
+    
+    sys.exit(1)
+
+    
+
+
+if __name__ == "__main__":
+    import sys, platform
+    from tabulate import tabulate
+    
+    with open("./credits.txt") as credits:
+        print(f"\n{credits.read()}");
+
+    os_table = [
+        ["Platform", platform.system()],
+        ["Version", platform.release()],
+        ["Arch", platform.machine()]
+    ]
+    print("\nOS INFORMATION")
+    print(tabulate(os_table))
+    
+
+    if len(sys.argv) > 1:
+        if "-net4" in sys.argv:
+            if "-hinfo" in sys.argv:
+                scan_host(sys.argv[sys.argv.index("-net4") + 1], True)
+            else:
+                scan_host(sys.argv[sys.argv.index("-net4") + 1], False)
+            
+    
+    print("\n[?] HostScan.py -net4 <netmask-ipv4> [-hinfo | -h/--help]")
+    sys.exit(0)
---
+++ README.md
@@ -0,0 +1,38 @@
+# **HOSTSCAN**
+### Easy to use command line network host scanner. *From noob to noobs.*
+## Dependencies
+- [Nmap 7.92](https://nmap.org/download.html) or superior
+- [Python 3.9](https://www.python.org/downloads/) or superior
+- All requirements written in [requirements.txt](https://github.com/NexCreep/HostScann/requiremets.txt)
+
+## Usage
+### Short Help
+```bash
+$~ python .\python .\HostScan.py
+```
+```bash
+$$\   $$\                       $$\      $$$$$$\
+$$ |  $$ |                      $$ |    $$  __$$\
+$$ |  $$ | $$$$$$\   $$$$$$$\ $$$$$$\   $$ /  \__| $$$$$$$\ $$$$$$\  $$$$$$$\
+$$$$$$$$ |$$  __$$\ $$  _____|\_$$  _|  \$$$$$$\  $$  _____|\____$$\ $$  __$$\
+$$  __$$ |$$ /  $$ |\$$$$$$\    $$ |     \____$$\ $$ /      $$$$$$$ |$$ |  $$ |
+$$ |  $$ |$$ |  $$ | \____$$\   $$ |$$\ $$\   $$ |$$ |     $$  __$$ |$$ |  $$ |
+$$ |  $$ |\$$$$$$  |$$$$$$$  |  \$$$$  |\$$$$$$  |\$$$$$$$\\$$$$$$$ |$$ |  $$ |
+\__|  \__| \______/ \_______/    \____/  \______/  \_______|\_______|\__|  \__|
+
+MADE WITH LOVE - N33xCr33p                                        Happy Hacking
+
+[!] THIS TOOL ONLY WORKS WITH IPV4 SUBNET
+
+OS INFORMATION
+--------  -------
+Platform  X
+Version   X
+Arch      X
+--------  -------
+
+[?] HostScan.py -net4 <netmask-ipv4> [-hinfo | -h/--help]
+```
+### Arguments
+- ```-net4 <netmask-ipv4>``` : Here you introduce the netmask address **after the net4** argument.
+- ```-hinfo``` : If you want more info from the host.
